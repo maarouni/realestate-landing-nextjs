492d3b5edc3e698b269aeb75d0eadafe61f73583
maarouni <maarouni@gmail.com>
2026-05-18 17:38:37 -0700

    Add sitemap.js for Google Search Console

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://realestate-analytics.ai',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+  ]
+}
