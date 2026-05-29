9c83c95333093a95e84efaebffaa470ff9eeeb5d
maarouni <maarouni@gmail.com>
2026-05-29 14:22:28 -0700

    Redirect non-www to www canonical

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'realestate-analytics.ai' }],
+        destination: 'https://www.realestate-analytics.ai/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
