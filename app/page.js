36222546b4148e728d2fc618c01cdb06f60a44b6
maarouni <maarouni@gmail.com>
2026-06-16 11:50:07 -0700

    Fix hero bg opacity, brighten nav links

--- app/page.js
+++ app/page.js
@@ -84,7 +84,7 @@ export default function Home() {
           backgroundImage: "url('https://images.unsplash.com/photo-1560518883-ce09059eeffa?w=1600&q=80')",
           backgroundSize: "cover",
           backgroundPosition: "center",
-          opacity: 0.18,
+          opacity: 0.35,
         }} />
 
         {/* Dark overlay gradient */}
